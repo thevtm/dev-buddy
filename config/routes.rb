f3ca8d9f7db7e3ad0af052c8789077e15b0b474e
Vinícius Tabille Manjabosco <tallibba@gmail.com>
2018-06-18 15:00:10 +0200

    feat: create user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  resources :users, only: [:index, :show, :new, :create, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
