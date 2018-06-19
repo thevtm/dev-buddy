151963ca8fde825eb326989a0f2049df77ecff39
Young Lee <younghoonlee1205@gmail.com>
2018-06-19 15:24:23 +0200

    Add the show page after user signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def index
   end
 
@@ -11,9 +11,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save!
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -21,4 +28,11 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :location, :profile_picture)
+  end
+
 end
