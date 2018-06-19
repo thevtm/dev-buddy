e2e3ea9633f64a42b8e9474f47a64fe8fb62dd72
david rousseau <david.rousseau06@gmail.com>
2018-06-19 16:36:52 +0200

    feat: implement index and show in the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,17 @@
 
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
   end
 
   def create
-  end
-
-  def show
   end
 
   def edit
