8c464df476a60f15dd9faa022a44336b34fb3454
Vinícius Tabille Manjabosco <tallibba@gmail.com>
2018-06-20 14:29:05 +0200

    feat: create simple navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   resources :matches, only: [:index, :create]
 
+  resources :contacts, only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
