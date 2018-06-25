20833b3de5c3331a1fee7f74e7a1b4d79cbaeea4
david rousseau <david.rousseau06@gmail.com>
2018-06-25 16:28:42 +0200

    add the swipe right

--- app/assets/javascripts/swipe_match.js
+++ app/assets/javascripts/swipe_match.js
@@ -1,5 +1,5 @@
 
-var myElement = document.getElementById('swiper');
+var myElement = document.querySelector('.match-profile');
 var myOptions = {}
 var matchButton = document.getElementById('match-button');
 
@@ -10,7 +10,12 @@ hammertime.on('swiperight',
   function(ev) {
      ev.preventDefault();
      myElement.classList.add('animated', 'rotateOutUpRight');
-     matchButton.submit()
+     Rails.fire(matchButton, "click")
 
   ;
 });
+
+// document.body.addEventListener('ajax:success', function(event) {
+//   var detail = event.detail;
+//   var data = detail[0], status = detail[1], xhr = detail[2];
+// })
