4313929035907970460286acf95eeb6c8fc226f1
david rousseau <david.rousseau06@gmail.com>
2018-06-25 17:54:56 +0200

    add the swipe left and the detection of the end of animation

--- app/assets/javascripts/swipe_match.js
+++ app/assets/javascripts/swipe_match.js
@@ -2,20 +2,35 @@
 var myElement = document.querySelector('.match-profile');
 var myOptions = {}
 var matchButton = document.getElementById('match-button');
-
+var notMatchButton = document.getElementById('not-match-button');
 
 var hammertime = new Hammer(myElement, myOptions);
 
-hammertime.on('swiperight',
+
+var swipeRight = hammertime.on('swiperight',
   function(ev) {
      ev.preventDefault();
-     myElement.classList.add('animated', 'rotateOutUpRight');
-     Rails.fire(matchButton, "click")
-
-  ;
+     myElement.classList.add('animated', 'rotateOutUpRight')
+      $('.match-profile').on('animationend', () => Rails.fire(matchButton, "click"));
 });
 
-// document.body.addEventListener('ajax:success', function(event) {
-//   var detail = event.detail;
-//   var data = detail[0], status = detail[1], xhr = detail[2];
-// })
+var swipeLeft = hammertime.on('swipeleft',
+  function(ev) {
+     ev.preventDefault();
+     myElement.classList.add('animated', 'rotateOutUpLeft')
+      $('.match-profile').on('animationend', () => location.reload());
+});
+
+
+
+
+
+
+
+
+// var afterSwipeLeft = function(ev) {
+//      Rails.fire(notMatchButton, "click");
+// };
+
+
+
