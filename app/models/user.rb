f44fe2fafaceec316897775ede35966f6ee31f91
david rousseau <david.rousseau06@gmail.com>
2018-06-28 13:04:45 +0200

    add profile mock information

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,48 @@ class User < ApplicationRecord
   mount_uploader :profile_picture, PhotoUploader
 
   has_many :matches, class_name: "Match", foreign_key: "user_a_id", dependent: :destroy
+
+  def job_position
+  end
+
+  def technology_summary
+  end
+
+  MOCK_DATA = {
+    job_position: {
+      :match_senior => {
+        'Senior Software Developer'
+      }
+
+      :match_junior => {
+        'Junior Ruby on Rails Developer'
+      }
+
+      :match_database => {
+        'Database Developer'
+      }
+    },
+    technology_summary: {
+      :match_senior => {
+        languages: %w[C++, C#, Java, Delphi, Visual Basic],
+        methodologies: %w[Rapid Application Design (RAD), Spiral Approach],
+        software: %w[TIBCO,  Apache, Business Objects],
+        systems: %w[Unix, Windows, VAX/VMS, NeXTStep],
+        databases: %w[Access, Sybase]
+      }
+      :match_junior => {
+        languages: %w[Ruby, SQL],
+        methodologies: %w[Prototyping Approach],
+        framework: %w[Rails, Bootstrap],
+      }
+
+      :match_database => {
+        languages: %w[C++, C#, Java, NodeJS, Visual Basic],
+        methodologies: %w[Agile, Waterfall Approach],
+        software: %w[Oracle WebLogic Server, Apache],
+        systems: %w[Unix, Windows, Net Framework, NeXTStep],
+        databases: %w[Oracle, MongoDB, SQL Server]
+      }
+    }
+  }
 end
